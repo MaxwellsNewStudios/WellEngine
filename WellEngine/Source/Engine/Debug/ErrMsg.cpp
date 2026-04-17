dac0ed1aee341382d98860903d40a81125b124cc
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-04-17 14:58:14 +0200

    Update Debug message indentation

--- WellEngine/Source/Engine/Debug/ErrMsg.cpp
+++ WellEngine/Source/Engine/Debug/ErrMsg.cpp
@@ -16,14 +16,23 @@ void MsgLogger::ErrorMessage(const char *msg, const std::string &filePath, int l
 {
 	MsgLogger &logger = GetInstance();
 	std::string indent = logger.GetIndent();
+    std::string msgStr = msg;
+
+    // Replace every '\n' with "\n" + indent
+    size_t pos = 0;
+    while ((pos = msgStr.find('\n', pos)) != std::string::npos)
+    {
+        msgStr.insert(pos + 1, indent);
+        pos += indent.length() + 1; // Move past the inserted indent and the newline
+    }
 
     std::string file = filePath.substr(filePath.find_last_of("/\\") + 1);
     OutputDebugString(StringUtils::NarrowToWide(indent + std::format("ERROR: [{}({})]\n", file, line)).c_str());
-    OutputDebugString(StringUtils::NarrowToWide(indent + msg).c_str());
+    OutputDebugString(StringUtils::NarrowToWide(indent + msgStr).c_str());
     OutputDebugString(L"\n");
 
 #undef cerr
-    std::cerr << indent << msg << std::endl;
+    std::cerr << indent << msgStr << std::endl;
 #define cerr CERR_USAGE_WARNING
 }
 void MsgLogger::ErrorMessage(const std::string &msg, const std::string &filePath, int line)
@@ -35,14 +44,23 @@ void MsgLogger::WarningMessage(const char *msg, const std::string &filePath, int
 {
     MsgLogger &logger = GetInstance();
     std::string indent = logger.GetIndent();
+    std::string msgStr = msg;
+
+    // Replace every '\n' with "\n" + indent
+    size_t pos = 0;
+    while ((pos = msgStr.find('\n', pos)) != std::string::npos)
+    {
+        msgStr.insert(pos + 1, indent);
+        pos += indent.length() + 1; // Move past the inserted indent and the newline
+    }
 
     std::string file = filePath.substr(filePath.find_last_of("/\\") + 1);
     OutputDebugString(StringUtils::NarrowToWide(indent + std::format("WARNING: [{}({})]\n", file, line)).c_str());
-    OutputDebugString(StringUtils::NarrowToWide(indent + msg).c_str());
+    OutputDebugString(StringUtils::NarrowToWide(indent + msgStr).c_str());
     OutputDebugString(L"\n");
 
 #undef cerr
-    std::cerr << indent << msg << std::endl;
+    std::cerr << indent << msgStr << std::endl;
 #define cerr CERR_USAGE_WARNING
 }
 void MsgLogger::WarningMessage(const std::string &msg, const std::string &filePath, int line)
@@ -54,9 +72,18 @@ void MsgLogger::DebugMessage(const char *msg, bool br)
 {
     MsgLogger &logger = GetInstance();
     std::string indent = logger.GetIndent();
+    std::string msgStr = msg;
+
+	// Replace every '\n' with "\n" + indent
+    size_t pos = 0;
+    while ((pos = msgStr.find('\n', pos)) != std::string::npos)
+    {
+        msgStr.insert(pos + 1, indent);
+        pos += indent.length() + 1; // Move past the inserted indent and the newline
+    }
 
 #undef cout
-    std::cout << indent << msg;
+    std::cout << indent << msgStr;
     if (br) std::cout << std::endl;
 #define cout COUT_USAGE_WARNING
 }
